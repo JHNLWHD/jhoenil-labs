2de81d59bc550fb6be598c43eb5738fa777e2fc0
JHNLWHD <aljhoenilw@gmail.com>
2025-05-24 20:21:19 +0800

    feat: integrate posthog for analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,20 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { PostHogProvider } from 'posthog-js/react'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+        debug: import.meta.env.MODE === "development",
+      }}
+    >
+      <App />
+    </PostHogProvider>
+  </React.StrictMode>
+);
